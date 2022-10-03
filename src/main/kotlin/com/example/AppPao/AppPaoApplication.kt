a1cb4b8316091f4c61866ca22e141c3b64efcc22
Elvis <elvisjulius@hotmail.com>
2022-10-03 17:59:18 -0300

    first commit

---
+++ src/main/kotlin/com/example/AppPao/AppPaoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.AppPao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppPaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppPaoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/AppPao/AppPaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.AppPao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppPaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
