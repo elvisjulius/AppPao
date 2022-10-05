a3f42f1e1b261c3a2dd751263d506a77552bd3b4
Elvis <elvisjulius@hotmail.com>
2022-10-05 14:49:09 -0300

    alterando model

--- src/main/kotlin/com/example/AppPao/Model/Product.kt
+++ src/main/kotlin/com/example/AppPao/Model/Product.kt
@@ -1,10 +1,15 @@
 package com.example.AppPao.Model
 
 import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
 
-
+@Entity
 data class Product(
+    @Id
     val productId: UUID,
+    @Column(nullable = false, length = 100)
     val name: String,
     val quantity: Int,
     val price: Long,
